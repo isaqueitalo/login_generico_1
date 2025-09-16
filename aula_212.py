364f43fc46dde1ec08a07341f91524a6a8735e8c
isaqueitalo <isaque.italo@hotmail.com>
2025-09-16 16:57:50 -0300

    criate_with_credentials

--- aula_212.py
+++ aula_212.py
@@ -1,4 +1,4 @@
-class Conection:
+class Connection:
 
     def __init__(self, host='localhost'):
         self.host = host
@@ -11,9 +11,18 @@ class Conection:
     def set_password(self, password):
         self.password = password
 
-c1 = Conection()
-c1.set_user('admin')
-c1.set_password('admin 123')
+    @classmethod
+    def criate_with_credentials(cls, user, password):
+        connection = cls()
+        connection.password = password
+        connection.user = user
+        return connection
+        
+
+c1 = Connection()
+c1 = Connection.criate_with_credentials('admin', 'admin 123')
+# c1.set_user('admin')
+# c1.set_password('admin 123')
 
 print(c1.user)
 print(c1.password)
